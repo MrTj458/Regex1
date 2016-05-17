c2ab922926a49ef8bb0dffa67381bf432952a5b3
Trevor <thod0127@740s-pl-130.local>
2016-05-17 13:26:15 -0600

    Added checkPhoneNumber & checkEmail & fixed checkLastName & added method to check all fields

--- src/view/Panel.java
+++ src/view/Panel.java
@@ -3,6 +3,7 @@ package view;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.InetAddress;
 
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
@@ -16,30 +17,33 @@ public class Panel extends JPanel
 {
 	private Controller baseController;
 	private SpringLayout baseLayout;
-	
+
 	private JTextField firstNameField;
 	private JTextField lastNameField;
 	private JTextField phoneNumberField;
 	private JTextField emailField;
-	
+
 	private JButton submitButton;
 	
+	private int numberPassed;
+
 	public Panel(Controller baseController)
 	{
 		this.baseController = baseController;
 		baseLayout = new SpringLayout();
-		
+
 		firstNameField = new JTextField("First Name");
 		lastNameField = new JTextField("Last Name");
 		phoneNumberField = new JTextField("Phone Number");
 		emailField = new JTextField("Email");
 		submitButton = new JButton("Submit");
-		
+		numberPassed = 0;
+
 		setupPanel();
 		setupLayout();
 		setupListeners();
 	}
-	
+
 	private void setupPanel()
 	{
 		this.setLayout(baseLayout);
@@ -50,7 +54,7 @@ public class Panel extends JPanel
 		this.add(emailField);
 		this.add(submitButton);
 	}
-	
+
 	private void setupLayout()
 	{
 		baseLayout.putConstraint(SpringLayout.SOUTH, submitButton, -10, SpringLayout.SOUTH, this);
@@ -68,30 +72,59 @@ public class Panel extends JPanel
 		baseLayout.putConstraint(SpringLayout.WEST, firstNameField, 10, SpringLayout.WEST, this);
 		baseLayout.putConstraint(SpringLayout.EAST, submitButton, -10, SpringLayout.EAST, this);
 	}
-	
+
 	private void setupListeners()
 	{
 		submitButton.addActionListener(new ActionListener()
 		{
 			public void actionPerformed(ActionEvent click)
-			{
-				if(checkFirstName())
-				{
-					JOptionPane.showMessageDialog(null, "First Name Pass");
-				}
-				if(checkLastName())
-				{
-					JOptionPane.showMessageDialog(null, "Last Name Pass");
-				}
+			{	
+				checkFields();
 			}
 		});
 	}
 	
+	private void checkFields()
+	{
+		boolean passed = true;
+		int failed = 0;
+		
+		if(!checkFirstName())
+		{
+			passed = false;
+			failed++;
+		}
+		if(!checkLastName())
+		{
+			passed = false;
+			failed++;
+		}
+		if(!checkEmail())
+		{
+			passed = false;
+			failed++;
+		}
+		if(!checkPhoneNumber())
+		{
+			passed = false;
+			failed++;
+		}
+		
+		if(passed)
+		{
+			JOptionPane.showMessageDialog(null, "All areas passed!");
+		}
+		else
+		{
+			JOptionPane.showMessageDialog(null, failed + " areas failed");
+		}
+	}
+
 	private boolean checkFirstName()
 	{
 		String name = firstNameField.getText();
-		
-		if(name.length() < 2 || name.length() > 30)
+
+		if (name.length() < 2 || name.length() > 30)
 		{
 			JOptionPane.showMessageDialog(this, "First name is too small or too big.");
 			return false;
@@ -99,25 +132,25 @@ public class Panel extends JPanel
 		else
 		{
 			char[] chars = name.toCharArray();
-			
-			for(char letter : chars)
+
+			for (char letter : chars)
 			{
-				if(!Character.isLetter(letter))
+				if (!Character.isLetter(letter))
 				{
 					JOptionPane.showMessageDialog(this, "First Name cannot contain symbols.");
 					return false;
 				}
 			}
-			
+
 			return true;
 		}
 	}
-	
+
 	private boolean checkLastName()
 	{
 		String name = lastNameField.getText();
-		
-		if(name.length() < 2 || name.length() > 40)
+
+		if (name.length() < 2 || name.length() > 40)
 		{
 			JOptionPane.showMessageDialog(this, "Last name is too small or too big.");
 			return false;
@@ -125,30 +158,62 @@ public class Panel extends JPanel
 		else
 		{
 			char[] chars = name.toCharArray();
-			
-			for(char letter : chars)
+
+			for (char letter : chars)
 			{
-				if(!Character.isLetter(letter))
+				if (!Character.isLetter(letter))
 				{
-					if(letter != '-' || letter != ',' || letter != '.')
+					if(letter == '-' || letter == ',' || letter == '.')
 					{
-						JOptionPane.showMessageDialog(this, "Last Name can only use - , . symbols");
+					}
+					else
+					{
+						JOptionPane.showMessageDialog(this, letter + " Last Name can only use - , . symbols");
 						return false;
 					}
 				}
 			}
-			
+
 			return true;
 		}
 	}
-	
-	private void checkPhoneNumber()
+
+	private boolean checkEmail()
 	{
-		
+		String email = emailField.getText();
+		String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+		java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+		java.util.regex.Matcher m = p.matcher(email);
+		if (m.matches())
+		{
+			return true;
+		}
+		else
+		{
+			JOptionPane.showMessageDialog(this, "email failed");
+			return false;
+		}
 	}
-	
-	private void checkEmail()
+
+	private boolean checkPhoneNumber()
 	{
-		
+		if (phoneNumberField.getText().length() == 10)
+		{
+			try
+			{
+				Long.parseLong(phoneNumberField.getText());
+				return true;
+			}
+			catch (Exception e)
+			{
+				JOptionPane.showMessageDialog(this, "Phone number must only contain numbers");
+				return false;
+			}
+		}
+		else
+		{
+			JOptionPane.showMessageDialog(this, "Phone number is not the correct length");
+			return false;
+		}
 	}
 }
